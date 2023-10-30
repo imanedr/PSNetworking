5952d11f48e8cd01dd542df636c56e3b95f1e518
imanedr <e.edrisian@gmail.com>
2023-10-30 16:34:26 -0500

    Updated Pin IP list

--- 1.0.0/functions/Ping-IpList.ps1
+++ 1.0.0/functions/Ping-IpList.ps1
@@ -10,7 +10,8 @@ function Ping-IpList
         [switch]$DontFragment,
         [int]$Ttl = 128,
         [int]$Timeout = 5000,
-        [switch]$Continuous
+        [switch]$Continuous,
+        [switch]$ShowHistory
     )
    
 
@@ -18,44 +19,100 @@ function Ping-IpList
     {
         if ($Continuous) { $Count = -1 }
         $iCount = 0
-   
-        while ($iCount -ne $Count)
-        {
-            $results = New-Object System.Collections.ArrayList
-            foreach ($ip in $ipList)
+        
+        if ($ShowHistory)
+        { 
+            $pingHistory = @{} 
+            $ipList | foreach { $pingHistory.Add($_, "") }
+            while ($iCount -ne $Count)
             {
-                $ping = New-Object System.Net.NetworkInformation.Ping
-                $pingOptions = New-Object System.Net.NetworkInformation.PingOptions($Ttl, $DontFragment)
-                $pingResult = $ping.Send($ip, $Timeout, [System.Text.Encoding]::ASCII.GetBytes(("a" * $BufferSize)), $pingOptions)
-
-                $pingStatistics = [pscustomobject]@{
-                    IPAddress   = ""
-                    ResponsTime = [double]0.0
-                    Result      = ""
-                }
-
-                if ($pingResult.Status -eq "Success")
+                foreach ($ip in $ipList)
                 {
-                    $pingStatistics.IPAddress = $ip
-                    $pingStatistics.ResponsTime = $pingResult.RoundtripTime
-                    $pingStatistics.Result = $pingResult.Status
-                    $results+= $pingStatistics
+                    $ping = New-Object System.Net.NetworkInformation.Ping
+                    $pingOptions = New-Object System.Net.NetworkInformation.PingOptions($Ttl, $DontFragment)
+                    $pingResult = $ping.Send($ip, $Timeout, [System.Text.Encoding]::ASCII.GetBytes(("a" * $BufferSize)), $pingOptions)
+    
+                    $pingStatistics = [pscustomobject]@{
+                        IPAddress   = ""
+                        ResponsTime = [double]0.0
+                        Result      = ""
+                    }
+    
+                    if ($pingHistory.item($ip).Result.length -eq 100 ){$pingHistory.item($ip).Result =""}
+                    if ($pingResult.Status -eq "Success")
+                    {
+                        $pingStatistics.IPAddress = $ip
+                        $pingStatistics.ResponsTime = $pingResult.RoundtripTime
+                        $pingStatistics.Result = $pingHistory.item($ip).Result + "!"
+                        $pingHistory.item($ip)= $pingStatistics
+                    
+                    }
+                    else
+                    {
+                        $pingStatistics.IPAddress = $ip
+                        $pingStatistics.ResponsTime = 9999
+                        $pingStatistics.Result = $pingHistory.item($ip).Result + "."
+                        $pingHistory.item($ip)= $pingStatistics
+                    }
+                }
+                Clear-Host
+                Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), Ping sequnce: $iCount"
+                Write-Host -ForegroundColor Green "IPAddress     ResponsTime  Result
+---------     -----------  ------"
+                foreach($item in $pingHistory.Values){
+                    Write-Host "$($item.IPAddress)           $($item.ResponsTime)  $($item.Result)"
+                }
+      
                 
-                }
-                else
-                {
-                    $pingStatistics.IPAddress = $ip
-                    $pingStatistics.ResponsTime = 9999
-                    $pingStatistics.Result = $pingResult.Status
-                    $results+= $pingStatistics
-                }
+                $iCount++
+                Start-Sleep -Seconds 1
             }
-            Clear-Host
-            Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), Ping sequnce: $iCount"
-            Write-Output $results | Format-Table -RepeatHeader
-            $iCount++
-            Start-Sleep -Seconds 1
+
         }
+        else
+        {
+            while ($iCount -ne $Count)
+            {
+                $results = New-Object System.Collections.ArrayList
+                foreach ($ip in $ipList)
+                {
+                    $ping = New-Object System.Net.NetworkInformation.Ping
+                    $pingOptions = New-Object System.Net.NetworkInformation.PingOptions($Ttl, $DontFragment)
+                    $pingResult = $ping.Send($ip, $Timeout, [System.Text.Encoding]::ASCII.GetBytes(("a" * $BufferSize)), $pingOptions)
+    
+                    $pingStatistics = [pscustomobject]@{
+                        IPAddress   = ""
+                        ResponsTime = [double]0.0
+                        Result      = ""
+                    }
+    
+    
+                    if ($pingResult.Status -eq "Success")
+                    {
+                        $pingStatistics.IPAddress = $ip
+                        $pingStatistics.ResponsTime = $pingResult.RoundtripTime
+                        $pingStatistics.Result = $pingResult.Status
+                        $results += $pingStatistics
+                    
+                    }
+                    else
+                    {
+                        $pingStatistics.IPAddress = $ip
+                        $pingStatistics.ResponsTime = 9999
+                        $pingStatistics.Result = $pingResult.Status
+                        $results += $pingStatistics
+                    }
+                }
+                Clear-Host
+                Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), Ping sequnce: $iCount"
+                Write-Output $results | Format-Table -RepeatHeader
+      
+                
+                $iCount++
+                Start-Sleep -Seconds 1
+            }
+        }
+        
     }
     finally
     {
